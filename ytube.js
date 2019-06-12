96fdd7fea41c8859589bf6c0ef056cb8cb4bf748
Eavenzz <47824114+Eavenzz@users.noreply.github.com>
2019-06-12 21:16:52 +0100

    Add files via upload

--- ytube.js
+++ ytube.js
@@ -1,57 +1,30 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-const ffmpeg = require ('fluent-ffmpeg');
-const moment = require('moment')
 // TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
 // TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
 // TypeScript: import ytdl = require('ytdl-core'); with neither of the above
 
 //ita 140-m4a, 250-Webm 70k , 171-WebM 128k
 
+//How to call function in main file
 function downloadVideo(videoURL){
   return new Promise((resolve,reject)=>{
+    let start = Date.now();
     validateURL(videoURL)
     .then((URL)=>{
-      console.log("valid URL")
-      return download(URL)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-    .then((message)=>{
-      console.log(message)
-      return convertmp3()
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-    .then((message)=>{
-      console.log(message)
-      console.log('yooo')
-      resolve("ready for speech text")
+      console.log("valid URL", "Downloading......")
+      videoStream = fs.createWriteStream('./public/video.mp4');
+      ytdl(URL)
+        .pipe(videoStream);
+      videoStream.on('close',() =>{
+        resolve(`Download took  ${Math.floor((Date.now()-start)/1000)} seconds`)  
+      })
     })
     .catch((err)=>{
       console.log(err)
     })
   });
 }
-
-function convertmp3(){
-    console.log("doing mp3 conversion")
-    return new Promise((resolve,reject)=>{
-      var proc = new ffmpeg({ source: './public/video.mp4', nolog: true })
-      proc.setFfmpegPath("./public/ffmpeg/bin/ffmpeg.exe")
-    .toFormat('mp3')
-     .on('end', function() {
-     resolve('file has been converted successfully');
-     })
-     .on('error', function(err) {
-     console.log('an error happened: ' + err.message);
-     })
-     // save to file <-- the new file I want -->
-     .saveToFile('./public/audio.mp3');     
-    })
-  }
 
 
 function validateURL(videoURL){
@@ -67,23 +40,7 @@ function validateURL(videoURL){
   })
 }
 
-function download(URL){
-  return new Promise((resolve,reject)=>{
-    let start = Date.now()
-    console.log(`Start on  ${start}`)
-    videoStream = fs.createWriteStream('./public/video.mp4');
-    ytdl(URL)
-      .pipe(videoStream);
 
-    videoStream.on('close',() =>{
-      resolve(`Download took  ${Math.floor((Date.now()-start)/1000)} seconds`)  
-    })
-    videoStream.on('error',(err)=>{reject(err)})
-  })
-}
-
-downloadVideo('https://www.youtube.com/watch?v=_B8RaLCNUZw')
-  .then((message)=>{console.log(message)})
 // Use to get video info for reference
 //  ytdl.getInfo('https://www.youtube.com/watch?v=qaIghx4QRN4&t=24s')
 //     .then(res => {
